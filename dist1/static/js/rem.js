ba3d7751436ff9654047f441f1e3e99ab64ac03d
ZH <2296362962@qq.com>
2018-11-05 09:47:47 +0800

    no message

---
+++ dist1/static/js/rem.js
@@ -0,0 +1,20 @@
+// 通过js适配不同的屏幕大小
+(function (doc, win) {
+    var html = doc.getElementsByTagName("html")[0],
+    //orientationchange ->手机屏幕转屏事件；
+    //resize->页面大小改变事件(一般pc端有用)
+      reEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+      reFontSize = function () {
+        var clientW = doc.documentElement.clientWidth||doc.body.clientWidth;
+        if (!clientW) {
+            return;
+        };
+        // f/320=80/375
+        html.style.fontSize=80 *(clientW/375) + "px";
+       
+      };
+      win.addEventListener(reEvt,reFontSize);
+      //DOMContentLoaded->dom加载完就执行，onload要dom/css/js都加载完才执行；
+     doc.addEventListener('DOMContentLoaded', reFontSize);
+  })(document, window);
+
